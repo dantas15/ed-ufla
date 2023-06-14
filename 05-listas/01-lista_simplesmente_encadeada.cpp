17afd1f3aa07eb5ec5e33b0bc129e6eca48d64c6
Gustavo Dantas Galote <gusgalot3g@gmail.com>
2023-06-14 16:22:07 -0300

    add list exercise

--- 05-listas/01-lista_simplesmente_encadeada.cpp
+++ 05-listas/01-lista_simplesmente_encadeada.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 
 
-struct equipe {
+struct equipe{
     string nomeEquipe;
     string lider;
     string linguagem; 
@@ -29,7 +29,7 @@ public:
 };
 
 // construindo time chamando seu construtor
-noh::noh(equipe d) {
+noh::noh(equipe d){
     elenco = d;
     proximo = NULL;
 }
@@ -88,63 +88,47 @@ lista::lista(const lista& umaLista) {
 }
 
 // destrutor da lista (chama função privada auxiliar)
-lista::~lista() {
-    this->removeTodos();
+lista::~lista( ) {
+
 }    
 
 // remove todos os elementos da lista
 void lista::removeTodos( ) {
-    noh* aux = this->primeiro;
-    noh* temp;
 
-    while(aux != NULL) {
-        temp = aux;
-        aux = aux->proximo;
-        delete temp;
-    }  
-
-    this->tamanho = 0;
-    this->primeiro = NULL;
-    this->ultimo = NULL;
 }    
 
 // sobrecarga do operador de atribuição
-lista& lista::operator=(const lista& umaLista) {
+lista& lista::operator=(const lista& umaLista){
     // limpa a lista atual
-    this->removeTodos();
-    // percorre a lista recebida como parâmetro, copiando os dados
+    removeTodos();
+     // percorre a lista recebida como parâmetro, copiando os dados
     noh* aux = umaLista.primeiro;
     
     while (aux != NULL) {
-        this->insereNoFim(aux->elenco);
+        insereNoFim(aux->elenco);
         aux = aux->proximo;  
     }
     
     return *this; 
 }  
 
-void lista::insere(equipe elenco) {
-    this->insereNoFim(elenco);
-}
-
 // insere no final da lista
 void lista::insereNoFim(equipe elenco) {
-    noh* novo = new noh(elenco);
+}
+
+// insere no início da lista
+void lista::insereNoInicio(equipe elenco) {
+
 
     if(this->vazia()) {
         this->primeiro = novo;
         this->ultimo = novo;
     } else {
-        this->ultimo->proximo = novo;
-        this->ultimo = novo;
+        novo->proximo = this->primeiro;
+        this->primeiro = novo;
     }
     this->tamanho++;
-}
-
-// insere no início da lista
-void lista::insereNoInicio(equipe elenco) {
-    noh* novo = new noh(elenco);
-
+ 
     if(this->vazia()) {
         this->primeiro = novo;
         this->ultimo = novo;
@@ -157,67 +141,53 @@ void lista::insereNoInicio(equipe elenco) {
 
 // insere em uma determinada posição da lista
 void lista::insereNaPosicao(int posicao, equipe elenco){
-    noh* novo = new noh(elenco);
-    if((posicao <= this->tamanho) and (posicao >= 0)) {
-        if(this->vazia()) {
-            this->primeiro = novo;
-            this->ultimo = novo;
-        } else if (posicao == 0) {
-            novo->proximo = primeiro;
-            this->primeiro = novo;
-        } else if (posicao == tamanho) {
-            this->ultimo->proximo = novo;
-            this->ultimo = novo;
-        } else {
-            noh* aux = this->primeiro;
-            int posAux = 0;
 
-            while( posAux < (posicao-1)) {
-                aux = aux->proximo;
-                posAux++;
-            }
-            novo->proximo = aux->proximo;
-            aux->proximo = novo;
-        }
-        this->tamanho++;
-    } else {
-        cerr << "Posição Inexistente!" << endl;
-        exit(EXIT_FAILURE);
-    }
 }
 
-// procura a posição de um determinado elemento
+//procura a posição de um determinado elemento
 int lista::procura(string valor) {
-    if (this->vazia()) {
+    if (vazia()) {
         throw runtime_error("Lista vazia!");
-    }
+    } 
 
-    noh* aux = primeiro;
-    int posAux = 0;
-
-    while((aux != NULL) and (aux->elenco.nomeEquipe != valor)) {
-        posAux++;
-        aux = aux->proximo;
-    }
-
-    if(aux == NULL) {
-        posAux = -1;
-    }
-
-    if (posAux == -1) {
-        throw runtime_error("Nao encontrado");
-    }
-
-    return posAux;
 }
 
 
 // método básico que *percorre* uma lista, imprimindo seus elementos
 // poderia percorrer fazendo outra ação (multiplicando valores, por exemplo)
 void lista::imprime() {
-    if (this->vazia()) {
+    if (vazia()) {
         throw runtime_error("Lista vazia!");
     }
+    }
+
+    noh* aux = primeiro;
+
+    while(aux != NULL) {
+        cout << "(" << aux->elenco.nomeEquipe << ", ";
+        cout << aux->elenco.lider << ", ";
+        cout << aux->elenco.linguagem << ", ";
+        cout << aux->elenco.qtdMembros << ")";
+        aux = aux->proximo;
+        cout << endl;
+    }
+}
+
+void lista::imprimeReverso() {
+    this->imprimeReversoAux(primeiro);
+    cout << endl;
+}
+
+
+void lista::imprimeReversoAux(noh* umNoh) {
+    if(umNoh != NULL) {
+        imprimeReversoAux(umNoh->proximo);
+        cout << "(" << umNoh->elenco.nomeEquipe << ", ";
+        cout << umNoh->elenco.lider << ", ";
+        cout << umNoh->elenco.linguagem << ", ";
+        cout << umNoh->elenco.qtdMembros << ")" << endl;
+    }
+    } 
 
     noh* aux = primeiro;
 
@@ -253,42 +223,9 @@ inline bool lista::vazia() {
 }
    
 void lista::removeNoFim() {
-     if (primeiro == NULL) {
-        // A lista está vazia, não há nada para remover
-        throw runtime_error("Remoção em lista vazia!");
-    } else if (primeiro->proximo == NULL) {
-        // A lista tem apenas um elemento, então remove o primeiro nó
-        delete primeiro;
-        primeiro = NULL;
-        ultimo = NULL;
-        return;
-    }
-
-    // Encontra o penúltimo nó
-    noh* penultimo = this->primeiro;
-    while (penultimo->proximo->proximo != NULL) {
-        penultimo = penultimo->proximo;
-    }
-
-    // Remove o último nó
-    delete this->ultimo;
-    this->ultimo = penultimo;
-    this->ultimo->proximo = NULL;
 }
     
 void lista::removeNoInicio() {
-    if(this->vazia()){
-        throw runtime_error("Remoção em lista vazia!");
-    }
-
-    noh* aux = primeiro;
-    this->primeiro = this->primeiro->proximo;
-    delete aux;
-
-    if (this->primeiro == NULL) {
-        // Se o primeiro era o único também atualiza o ponteiro de último
-        ultimo = NULL;
-    }
 }
 
 int main() {
